1b81295455f7566d81b7749e686fb32deb9c4961
zengzhaojin <742765643@qq.com>
2020-10-09 11:23:26 +0800

    修改

--- skywalking-ui/src/utils/services.ts
+++ skywalking-ui/src/utils/services.ts
@@ -1,67 +1,3 @@
-<<<<<<< HEAD
-import request from '@/utils/request';
-
-const dataService = {
-  getMonitorViewVo() {
-    return new Promise((resolve, reject) => {
-      request({
-        url: '/main/getMonitorViewVo',
-        method: 'get',
-      }).then((res) => {
-          resolve(res);
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  },
-
-  getAlarmDistribution() {
-    return new Promise((resolve, reject) => {
-      request({
-        url: '/main/getAlarmDistribution',
-        method: 'get',
-      }).then((res) => {
-          resolve(res);
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  },
-
-  getAlarm() {
-    return new Promise((resolve, reject) => {
-      request({
-        url: '/main/getAlarm',
-        method: 'get',
-      })
-        .then((res) => {
-          resolve(res);
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  },
-
-  getDeviceStatus() {
-    return new Promise((resolve, reject) => {
-      request({
-        url: '/main/getDeviceStatus',
-        method: 'get',
-      }).then((res) => {
-          resolve(res);
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  },
-};
-
-export default dataService;
-=======
 import axios from 'axios';
 
 let instance = axios.create({
@@ -124,4 +60,3 @@ const dataService = {
 };
 
 export default dataService;
->>>>>>> 0606a8a8326bc1e09138c0baeb84ace7da4011a6
